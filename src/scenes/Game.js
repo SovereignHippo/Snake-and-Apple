769fbfeee76e20e9c02f89a434171bc3b38ae4da
Zachary Mueller <zacharymueller3208@gmail.com>
2026-01-14 22:07:40 -0600

    Added the snake

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -5,19 +5,34 @@ export class Game extends Phaser.Scene {
 
         this.playingField = {width: 40, height: 20};
         this.wallWidth = 30;
-        this.fieldStartingPosition = {x: 50, y: 50};
+        this.fieldStartingPosition = {x: 60, y:60};
         this.fieldColors = {colorA: 0x303070, colorB: 0x000000, wallColor: 0x808080}
+        this.isGameOver;
         
 
-
-        this.appleSize = 30;
-        this.isAppleNoMoveBeingPressed = false;
-        this.appleMoveDistance = this.appleSize;
-        this.appleStartingCords = {x:5, y: 9};
-        this.appleCurrentCords;
-        this.appleOnMovementCoolDown = false;
-        this.appleMovementCoolDown = 60;
-        this.appleMovementCoolDownTime = 0;
+        this.apple = {
+            size: 28,
+            isNoMoveBeingPressed: false,
+            moveDistance: this.wallWidth,
+            startingCords: {x:5, y:9},
+            currentCords: {x:5, y:9},
+            isOnMovementCoolDown: false,
+            movementCoolDown:60,
+            movementCoolDownTime:0,
+            object: undefined,
+        };
+        
+        this.snake = {
+            body: [],
+            size: 28,
+            direction: 'left',
+            nextDirection: 'left',
+            speed: 150, //Higher number actually means slower snake
+            lastMoveTime: 0,
+            startingNumberOfSegments: 3,
+            startingCords: {x:30, y: 9},
+            currentCords: {x:30, y: 9},
+        };
 
 
         this.time;
@@ -34,6 +49,7 @@ export class Game extends Phaser.Scene {
 
     create() {
         // Create game objects
+        this.isGameOver = false;
 
         //Build the field
             this.field = Array.from({ length: this.playingField.width }, () => new Array(this.playingField.height).fill('0'));
@@ -73,13 +89,28 @@ export class Game extends Phaser.Scene {
                     this.add.rectangle(this.fieldStartingPosition.x + (i * this.wallWidth),this.fieldStartingPosition.y  + (j * this.wallWidth),this.wallWidth,this.wallWidth,color);
                 }
             }
-        
+            
         
        
         //Create the apple instnace
-            this.apple = this.add.rectangle(this.fieldStartingPosition.x + (this.appleStartingCords.x * this.wallWidth),this.fieldStartingPosition.y + (this.appleStartingCords.y * this.wallWidth),this.appleSize,this.appleSize,0xff0000);
-            this.field[this.appleStartingCords.x][this.appleStartingCords.y] = 'a';
-            this.appleCurrentCords = this.appleStartingCords;
+            this.apple.object = this.add.rectangle(this.fieldStartingPosition.x + (this.apple.startingCords.x * this.wallWidth),this.fieldStartingPosition.y + (this.apple.startingCords.y * this.wallWidth),this.apple.size,this.apple.size,0xff0000);
+            this.field[this.apple.startingCords.x][this.apple.startingCords.y] = 'a';
+            this.apple.currentCords = this.apple.startingCords;
+
+        //Create the snake instnace
+            this.snake.body = [];
+            
+
+            for(let i = 0; i < this.snake.startingNumberOfSegments; i++){
+                const segment = this.add.rectangle(this.fieldStartingPosition.x + (this.snake.startingCords.x * this.wallWidth) + (i * this.wallWidth),this.fieldStartingPosition.y + (this.snake.startingCords.y * this.wallWidth),this.snake.size,this.snake.size,0x0000ff);
+                const bodyPart = {
+                    currentCords: {x:this.snake.startingCords.x + i, y: this.snake.startingCords.y},
+                    object: segment,
+                }
+                this.field[bodyPart.currentCords.x][bodyPart.currentCords.y] = 's';
+                this.snake.body.push(bodyPart);
+            }
+            
 
         //Combo manager
             //Sets the combos
@@ -93,10 +124,11 @@ export class Game extends Phaser.Scene {
                     
                 })
 
-                this.cursors = this.input.keyboard.createCursorKeys();
+               
 
          
         //Apple input manager
+
             var up = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
             var down = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
             var left = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
@@ -131,7 +163,7 @@ export class Game extends Phaser.Scene {
                 
                 //this checks to make sure its the left ctr key and not the right one
                 if(event.location != 1) return;
-                this.isAppleNoMoveBeingPressed = false;
+                this.apple.isNoMoveBeingPressed = false;
                 
                 //BUG FOUND if the left ctr is being held and the right control is pressed the left control will not unpress
 
@@ -141,13 +173,18 @@ export class Game extends Phaser.Scene {
                 
                 //this checks to make sure its the left ctr key and not the right one
                 if(event.location != 1) return;
-                this.isAppleNoMoveBeingPressed = true;
+                this.apple.isNoMoveBeingPressed = true;
                 
 
 
             });
 
-       
+        //Sanke input manager
+         this.cursors = this.input.keyboard.createCursorKeys();
+
+         
+        //console.log(this.field);
+   
     }
 
     
@@ -157,6 +194,21 @@ export class Game extends Phaser.Scene {
         this.time = time;
         
         
+        if (this.cursors.left.isDown && this.snake.direction !== 'right') {
+            this.snake.nextDirection = 'left';
+        } else if (this.cursors.right.isDown && this.snake.direction !== 'left') {
+            this.snake.nextDirection = 'right';
+        } else if (this.cursors.up.isDown && this.snake.direction !== 'down') {
+            this.snake.nextDirection = 'up';
+        } else if (this.cursors.down.isDown && this.snake.direction !== 'up') {
+            this.snake.nextDirection = 'down';
+        }
+
+        // Move snake at fixed intervals
+        if (time >= this.snake.lastMoveTime + this.snake.speed) {
+            this.MoveSnake();
+            this.snake.lastMoveTime = time;
+        }
             
         
        
@@ -170,20 +222,121 @@ export class Game extends Phaser.Scene {
         //Sets apple's sprite based on direction
 
         //Check if control is being held
-        if(this.isAppleNoMoveBeingPressed) return;
+        if(this.apple.isNoMoveBeingPressed) return;
 
         //Check if the apple is allowed to move where it want to
-        if(this.field[this.appleCurrentCords.x + xDirection][this.appleCurrentCords.y + yDirection] != '0')return;
+        if(this.field[this.apple.currentCords.x + xDirection][this.apple.currentCords.y + yDirection] != '0')return;
 
         //Moves Apple
-        if(this.time >= this.appleMovementCoolDownTime + this.appleMovementCoolDown){
-            this.apple.destroy();
-            this.apple = this.add.rectangle(this.apple.x + (this.appleMoveDistance * xDirection),this.apple.y + (this.appleMoveDistance * yDirection),this.appleSize,this.appleSize,0xff0000);
-            this.appleMovementCoolDownTime = this.time;
-            this.appleCurrentCords = {x:this.appleCurrentCords.x + xDirection,y: this.appleCurrentCords.y + yDirection};
+        if(this.time >= this.apple.movementCoolDownTime + this.apple.movementCoolDown){
+            //Physically move the apple
+            this.apple.object.destroy();
+            this.field[this.apple.currentCords.x][this.apple.currentCords.y] = "0";
+
+            this.apple.object = this.add.rectangle(this.apple.object.x + (this.apple.moveDistance * xDirection),this.apple.object.y + (this.apple.moveDistance * yDirection),this.apple.size,this.apple.size,0xff0000);
+            this.apple.movementCoolDownTime = this.time;
+            this.apple.currentCords = {x:this.apple.currentCords.x + xDirection,y: this.apple.currentCords.y + yDirection};
+            this.field[this.apple.currentCords.x][this.apple.currentCords.y] = "a";
+
+           
+            
         }
 
         
+    }
+
+    MoveSnake() {
+         // Update current direction
+        this.snake.direction = this.snake.nextDirection;
+
+        // Calculate new head position
+        const head = this.snake.body[0].object;
+        let newX = head.x;
+        let newY = head.y;
+
+        let newCordX = this.snake.body[0].currentCords.x;
+        let newCordY = this.snake.body[0].currentCords.y;
+
+
+        switch (this.snake.direction) {
+            case 'left':
+                newX -= this.wallWidth;
+                newCordX -= 1;
+                break;
+            case 'right':
+                newX += this.wallWidth;
+                newCordX += 1;
+                break;
+            case 'up':
+                newY -= this.wallWidth;
+                newCordY -= 1;
+                break;
+            case 'down':
+                newY += this.wallWidth;
+                newCordY += 1;
+                break;
+        }
+
+
+        //Check for collision
+             let isEating = false;
+            switch(this.field[newCordX][newCordY]){
+                case 'a':
+                    isEating = true;
+                break;
+                case 'w':
+                    this.GameOver();
+                    return;
+                case 's':
+                    this.GameOver();
+                    return;
+            }
+
+        //Move snake 
+            //by making a new head
+            const newHead = this.add.rectangle(newX,newY,this.snake.size,this.snake.size,0x0000ff);
+            const newHeadObject = {
+                currentCords: {x:newCordX, y: newCordY},
+                object: newHead,
+            }
+            this.field[newHeadObject.currentCords.x][newHeadObject.currentCords.y] = 's';
+            this.snake.body.unshift(newHeadObject);
+            
+
+            //check as to weather or not it needs to grow
+            if(!isEating){
+                //Remove tail if not eating
+                const tail = this.snake.body.pop();
+                this.field[tail.currentCords.x][tail.currentCords.y] = '0';
+                tail.object.destroy();
+            }else{
+                //Apple has been eaten
+                this.apple.object.destroy();
+                this.RespawnApple();
+                //Need to add logic
+            }
+        
+    }
+
+    RespawnApple(){
+        const x = Math.floor(Math.random() * (this.playingField.width - 2));
+        const y = Math.floor(Math.random() * (this.playingField.height - 2));
+
+        if(this.field[x][y] != '0'){
+            this.RespawnApple();
+            return;
+        }
+
+        this.field[x][y] = 'a';
+        this.apple.currentCords = {x: x, y: y};
+        this.apple.object = this.add.rectangle(this.fieldStartingPosition.x + (x * this.wallWidth), this.fieldStartingPosition.y + (y * this.wallWidth),this.apple.size,this.apple.size,0xff0000);
+
+    }
+
+    GameOver(){
+        if(this.isGameOver) return;
+        this.isGameOver = true;
+        console.log('Game Over!');
     }
 
 
